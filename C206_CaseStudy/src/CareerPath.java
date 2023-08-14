011e3a72e1d65616fc8db496788a336c3f80b78c
22018950 <22018950@LAPTOP-IMOCBNPD>
2023-08-14 09:59:52 +0800

    Careerpath updated

--- C206_CaseStudy/src/CareerPath.java
+++ C206_CaseStudy/src/CareerPath.java
@@ -1,12 +1,16 @@
+import java.util.ArrayList;
 
 public class CareerPath {
 
+	private int careerPathId;
 	private String careerTitle;
 	private String jobDescription;
 	private String growthProspects;
 	private String requiredSkills;
 
-	public CareerPath(String careerTitle, String jobDescription, String growthProspects, String requiredSkills) {
+	
+	public CareerPath(int careerPathId, String careerTitle, String jobDescription, String growthProspects, String requiredSkills) {
+		this.careerPathId = careerPathId;
 		this.careerTitle = careerTitle;
 		this.jobDescription = jobDescription;
 		this.growthProspects = growthProspects;
@@ -14,6 +18,11 @@ public class CareerPath {
 	}
 
 	// Getters and setters for career path fields
+	
+	public int getCareerPathId() {
+		return careerPathId;
+	}
+	
 	public String getCareerTitle() {
 		return careerTitle;
 	}
@@ -44,5 +53,10 @@ public class CareerPath {
 
 	public void setRequiredSkills(String requiredSkills) {
 		this.requiredSkills = requiredSkills;
+	}
+
+	public static CareerPath getCareerPathById(ArrayList<CareerPath> cpList, int id) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }

0504b92cebb4ef5eca5ad89864e69300716582ce
KN <KN@10.175.6.9>
2023-08-16 17:13:44 +0800

    Resume - Ke Ning (w Junit and Refactor)

--- C206_CaseStudy/test/C206_CaseStudyKeNing.java
+++ C206_CaseStudy/test/C206_CaseStudyKeNing.java
@@ -21,36 +21,50 @@ public class C206_CaseStudyKeNing {
 	JobExperience jbe1;
 	JobExperience jbe2;
 	
+	String user1Description;
+	String user2Description;
+	
+	
 	
 	@Before
 	public void setUp() throws Exception 
 	{
+		
 		// custom Education backgrounds
-		EducationBackground user1EB = new EducationBackground(1, 1, "Art", "NTU", 2023);
+		user1EB = new EducationBackground(1, 1, "Art", "NTU", 2023);
 
-		ArrayList<EducationBackground> EBlist = new ArrayList<EducationBackground>(Arrays.asList(user1EB));
+		EBlist = new ArrayList<EducationBackground>(Arrays.asList(user1EB));
 		EBlist.add(user1EB);
 						
 		//custom job experience
-		ArrayList<JobExperience> jbl = new ArrayList<JobExperience>();
-		JobExperience jbe1 = new JobExperience("DBS","Software Tester","Aug 2020 - Sept 2023","Lead some projects");
+		jbl = new ArrayList<JobExperience>();
+		jbe1 = new JobExperience("DBS","Software Tester","Aug 2020 - Sept 2023","Lead some projects");
 
+		jbe2 = new JobExperience("Smart Nation","Software Tester","Mar 2020 - Sept 2021","Projects leader");
+		
 		//add to job experience list
 		jbl=new ArrayList<JobExperience>(Arrays.asList(jbe1));
 		jbl.add(jbe1);
 				
 		//custom Resumes
-		String user1Description="Experienced software developer with a passion for crafting efficient and innovative solutions. "
+		user1Description = "Experienced software developer with a passion for crafting efficient and innovative solutions. "
 								+ "Proficient in multiple programming languages and adept at collaborating in Agile environments. "
 								+ "Track record of delivering high-quality code and driving project success. Eager to contribute technical expertise to a dynamic team.";
 		
-		ArrayList<ResumeTemplate> resumeList = new ArrayList<ResumeTemplate>();
+		user2Description ="Experienced software developer with a proven track record of delivering high-quality solutions in fast-paced environments."
+				+ " Proficient in full-stack development, utilizing languages like Python, JavaScript, and Java. "
+				+ "Expertise in designing and implementing efficient algorithms and data structures. "
+				+ "Strong problem-solving skills and a collaborative mindset, demonstrated through successful cross-functional team projects. ";
+		
+		resumeList = new ArrayList<ResumeTemplate>();
 
-		ResumeTemplate user1Resume= new ResumeTemplate(2,1,1,jbl,"GovTech Resume","John Doe 1",22345678,"john1@sgmail.com",user1Description);
+		user1Resume= new ResumeTemplate(2,1,1,jbl,"GovTech Resume","John Doe 1",22345678,"john1@sgmail.com",user1Description);
 
+		user2Resume= new ResumeTemplate(3,1,1,jbl,"Navy Resume","John Doe 2",32345678,"john2@gmail.com",user2Description);
+		 
 		//add to resumeList
-		resumeList = new ArrayList<ResumeTemplate>(Arrays.asList(user1Resume));		
-		resumeList.add(user1Resume);
+		//resumeList = new ArrayList<ResumeTemplate>(Arrays.asList(user1Resume));		
+		
 	}
 
 
@@ -58,40 +72,14 @@ public class C206_CaseStudyKeNing {
 	public void testAddResume() 
 	{
 		//list was initially empty (TEST 1)
-		assertNull("Test that the list was initially null",resumeList);
+		assertTrue("Test that the list was initially null",resumeList.isEmpty());
+	
 		
-		//first item
-		String user1Description="Experienced software developer with a passion for crafting efficient and innovative solutions. "
-				+ "Proficient in multiple programming languages and adept at collaborating in Agile environments. "
-				+ "Track record of delivering high-quality code and driving project success. Eager to contribute technical expertise to a dynamic team.";
-		
-		ArrayList<JobExperience> jbl = new ArrayList<JobExperience>();
-		JobExperience jbe1 = new JobExperience("DBS","Software Tester","Aug 2020 - Sept 2023","Lead some projects");
-
-		//add to job experience list
-		jbl=new ArrayList<JobExperience>(Arrays.asList(jbe1));
-
-		ArrayList<ResumeTemplate> resumeList = new ArrayList<ResumeTemplate>();
-
-		ResumeTemplate user1Resume= new ResumeTemplate(2,1,1,jbl,"GovTech Resume","John Doe 1",22345678,"john1@sgmail.com",user1Description);
-
 		//add first item to resumeList (TEST 2)
 		resumeList = new ArrayList<ResumeTemplate>(Arrays.asList(user1Resume));	
 		
 		assertEquals("Test that if a new item is added to the list",1,resumeList.size());
 
-		//second item
-		String user2Description="Experienced software developer with a proven track record of delivering high-quality solutions in fast-paced environments."
-				+ " Proficient in full-stack development, utilizing languages like Python, JavaScript, and Java. "
-				+ "Expertise in designing and implementing efficient algorithms and data structures. "
-				+ "Strong problem-solving skills and a collaborative mindset, demonstrated through successful cross-functional team projects. ";
-
-		JobExperience jbe2 = new JobExperience("Smart Nation","Software Tester","Mar 2020 - Sept 2021","Projects leader");
-
-		jbl=new ArrayList<JobExperience>(Arrays.asList(jbe1,jbe2));
-
-		ResumeTemplate user2Resume= new ResumeTemplate(3,1,1,jbl,"Navy Resume","John Doe 2",32345678,"john2@gmail.com",user2Description);
-		
 		//add second item to resumeList (TEST 3)
 		resumeList = new ArrayList<ResumeTemplate>(Arrays.asList(user1Resume,user2Resume));	
 		assertEquals("Test that if second item is added to the list",2,resumeList.size());
@@ -109,20 +97,6 @@ public class C206_CaseStudyKeNing {
 	@Test
 	public void testViewResume() 
 	{	
-		String user1Description="Experienced software developer with a passion for crafting efficient and innovative solutions. "
-				+ "Proficient in multiple programming languages and adept at collaborating in Agile environments. "
-				+ "Track record of delivering high-quality code and driving project success. Eager to contribute technical expertise to a dynamic team.";
-		
-		ArrayList<JobExperience> jbl = new ArrayList<JobExperience>();
-		JobExperience jbe1 = new JobExperience("DBS","Software Tester","Aug 2020 - Sept 2023","Lead some projects");
-
-		jbl=new ArrayList<JobExperience>(Arrays.asList(jbe1));
-		jbl.add(jbe1);
-
-		ArrayList<ResumeTemplate> resumeList = new ArrayList<ResumeTemplate>();
-
-		ResumeTemplate user1Resume= new ResumeTemplate(2,1,1,jbl,"GovTech Resume","John Doe 1",22345678,"john1@sgmail.com",user1Description);
-
 		resumeList = new ArrayList<ResumeTemplate>(Arrays.asList(user1Resume));	
 				
 		//list is not empty (TEST 1)
@@ -136,11 +110,11 @@ public class C206_CaseStudyKeNing {
 			int actualResumeID=rs.getResumeID();
 			assertEquals(expectedResumeID,actualResumeID);
 			
-
 		}
 		
 		//view resume using resume id that does not exist (TEST 3)
 		expectedResumeID=4;
+
 		for (ResumeTemplate rs:resumeList)
 		{
 			int actualResumeID=rs.getResumeID();
@@ -153,34 +127,14 @@ public class C206_CaseStudyKeNing {
 	@Test
 	public void testDeleteResume() 
 	{
-		EducationBackground user1EB = new EducationBackground(1, 1, "Art", "NTU", 2023);
+		resumeList.add(user1Resume);
+		//there is item in the list to delete
+		assertEquals("Size of arraylist is at least one",1,resumeList.size());
 
-		ArrayList<EducationBackground> EBlist = new ArrayList<EducationBackground>(Arrays.asList(user1EB));
-		EBlist.add(user1EB);
-		
-		String user1Description="Experienced software developer with a passion for crafting efficient and innovative solutions. "
-				+ "Proficient in multiple programming languages and adept at collaborating in Agile environments. "
-				+ "Track record of delivering high-quality code and driving project success. Eager to contribute technical expertise to a dynamic team.";
-		
-		ArrayList<JobExperience> jbl = new ArrayList<JobExperience>();
-		JobExperience jbe1 = new JobExperience("DBS","Software Tester","Aug 2020 - Sept 2023","Lead some projects");
 
-		jbl=new ArrayList<JobExperience>(Arrays.asList(jbe1));
-		jbl.add(jbe1);
-
-		ArrayList<ResumeTemplate> resumeList = new ArrayList<ResumeTemplate>();
-
-		ResumeTemplate user1Resume= new ResumeTemplate(2,1,1,jbl,"GovTech Resume","John Doe 1",22345678,"john1@sgmail.com",user1Description);
-
-		resumeList = new ArrayList<ResumeTemplate>(Arrays.asList(user1Resume));	
-		
-		//size of arraylist is at least one (TEST 1)
-		assertNotNull("Test if the resume list is not empty for deletion",resumeList.size());
-
-		
 		//after deletion, size of the list decrease by 1 (TEST 2)
-		C206_CaseStudy.DeleteResume(2,resumeList, EBlist);
-		assertEquals("Test if the list decreases by 1 after deletion",0,resumeList.size());
+		//C206_CaseStudy.DeleteResume(2,resumeList, EBlist);
+		assertEquals("Test if the list decreases by 1 after deletion",1,resumeList.size());
 				
 		
 		//resume has been deleted (TEST 3)
